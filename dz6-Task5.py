118b90625ff881cabdbe53f9ddd83589d56f7825
Vzun <49799538+vzun88@users.noreply.github.com>
2022-09-25 19:57:30 +0300

    Update dz6-Task5.py

--- dz6-Task5.py
+++ dz6-Task5.py
@@ -1,17 +1,19 @@
 # Проверить все ли объекты имеют одинаковое значение некоторой характеристики. Написать функцию same_by(chara,objects):
 
-values=[0,2,4,6]
+values=[]
 values1=[0,2,3,4,6]
 
 characteristic=(lambda x: x%2)
 
 # Функция проверки на одинаковые возвращаемые характеристики объектов
 def same_by(chara,objects):
-    for i in range(len(objects)):
-        if chara(objects[0])!=chara(objects[i]): 
-            print(False)
-            break
-        if i==len(objects)-1:
-            print(True)
+    if not objects: print(True)
+    else:
+        for i in range(len(objects)):
+            if chara(objects[0])!=chara(objects[i]): 
+                print(False)
+                break
+            if i==len(objects)-1:
+                print(True)
 
 same_by(characteristic,values)
